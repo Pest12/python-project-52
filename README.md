fe81c8057db2910002ac11b24f94abe350f62b7c
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2024-02-06 18:13:44 +0500

    update README and settings

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Pest12/python-project-52/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Pest12/python-project-52/actions)
+
+
+##Link to Render.com: https://task-manager-app-hudw.onrender.com/
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'webserver']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://task-manager-app-hudw.onrender.com/', 'webserver']
 
 
 # Application definition
