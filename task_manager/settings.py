bdaac552079a661424270a0ed306052b0f67a212
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2024-03-13 17:20:05 +0500

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -89,8 +89,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
+        # Feel free to alter this value to suit your needs.
+        default=DATABASE_URL,
+        conn_max_age=600
     )
 }
 
