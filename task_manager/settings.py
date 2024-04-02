eaf40ccb37d0794ff3947df602e8420df190bc9b
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2024-04-02 16:58:24 +0500

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'webserver', '*']
+ALLOWED_HOSTS = ['127.0.0.1', 'webserver', 'task-manager-app-hudw.onrender.com']
 
 
 # Application definition
